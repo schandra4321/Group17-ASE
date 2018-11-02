1ab12573230b0e4b627fdf73e208d449f83d7e3a
apk_2000 <pavankumar.a17@iiits.in>
2018-11-02 11:48:26 +0530

    Added footer to homepage and linked register page

--- ase17/Auctions/urls.py
+++ ase17/Auctions/urls.py
@@ -2,5 +2,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='Auctions-home'),
+    path('', views.index, name='index'),
+    # path('dashboard/', views.dashboard, name='dashboard'),
+    # path('create-auction', views.create_auction, name='create-auction'),
+    # path('enter-details/', views.enter_details, name = 'enter-details'),
+    # path('dashboard/notifications/', views.notifications, name = 'notifications'),
+    # path('dashboard/cart/', views.cart, name = 'cart'),
+    # path('dashboard/created-auctions/', views.view_created, name = 'view-created'),
+    # path('dashboard/participating-auctions/', views.view_participating, name = 'view-participating'),
 ]
--- ase17/Auctions/views.py
+++ ase17/Auctions/views.py
@@ -3,5 +3,5 @@ from django.http import HttpResponse
 
 
 # Create your views here.
-def home(request):
+def index(request):
     return render(request, "Auctions/home.html")
